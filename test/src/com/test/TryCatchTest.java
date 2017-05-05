7e6f4ae13cf946b4a78f69de94556e0e792d4803
Ryan Guo <guohao_lin@msn.com>
2017-05-04 17:11:33 -0700

    try-catch-finally
    throws Exception

---
+++ test/src/com/test/TryCatchTest.java
@@ -0,0 +1,31 @@
+package com.test;
+
+public class TryCatchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TryCatchTest tct = new TryCatchTest();
+		int result = tct.test();
+		System.out.println("this is the end of test(), result value equals:" + result);
+	}
+
+	public int test(){
+		int divider = 10;
+		int result = 100;
+		try{
+			while(divider > -1){
+				divider--;
+				result = result +100/divider;
+						
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Warning: throws an exception");
+			return result = 999;
+		}finally{
+			System.out.println("The finally statement is executed, result value:" + result);
+		}
+		return result;
+	}
+}
